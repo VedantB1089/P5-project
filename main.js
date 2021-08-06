78db9f94680f030583b1b050d7408c7dee241832
VedantB1089 <77881640+VedantB1089@users.noreply.github.com>
2021-08-06 15:52:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,29 +6,31 @@ function preload() {
            video = createCapture(VIDEO);
             video.hide(); tint_color = ""; 
         }
-        function draw() {
-            image(video, 230, 150, 220, 200);
-          
-            fill(255, 0, 0);
-            stroke(255, 0, 0);
-            circle(50, 50, 80);
-          
-            circle(50, 430, 80);
-          
-            circle(590, 50, 80);
-          
-            circle(590, 430, 80);
-          
-            fill(0, 128, 0);
-            stroke(0, 128, 0);
-            rect(90, 40, 460, 20);
-          
-            rect(90, 420, 460, 20);
-          
-            rect(40, 90, 20, 300);
-          
-            rect(580, 90, 20, 300);
-          }
+         function draw() {
+              image(video, 0, 0, 600, 600);
+               tint(tint_color);
+              
+                fill(255, 0, 0);
+                stroke(255, 0, 0);
+                circle(50, 50, 80);
+              
+                circle(50, 430, 80);
+              
+                circle(590, 50, 80);
+              
+                circle(590, 430, 80);
+              
+                fill(0, 128, 0);
+                stroke(0, 128, 0);
+                rect(90, 40, 460, 20);
+              
+                rect(90, 420, 460, 20);
+              
+                rect(40, 90, 20, 300);
+              
+                rect(580, 90, 20, 300);
+              }
+             
              function take_snapshot(){
                   save('student_name.png');
                  }
